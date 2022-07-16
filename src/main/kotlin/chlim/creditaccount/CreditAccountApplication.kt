9108153bd929777920951ce12e74695402f990ba
chaehwanlim <chlim428@gmail.com>
2022-07-16 16:08:24 +0900

    initial commit

---
+++ src/main/kotlin/chlim/creditaccount/CreditAccountApplication.kt
@@ -0,0 +1,11 @@
+package chlim.creditaccount
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditAccountApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditAccountApplication>(*args)
+}
---
+++ src/test/kotlin/chlim/creditaccount/CreditAccountApplicationTests.kt
@@ -0,0 +1,13 @@
+package chlim.creditaccount
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAccountApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
