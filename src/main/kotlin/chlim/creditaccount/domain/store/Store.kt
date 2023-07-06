89fe123518b1333b386c593271f1c6b9249add8a
Harry(해리) <96237654+harry-class101@users.noreply.github.com>
2023-07-06 17:23:13 +0900

    feat: add Store entity

---
+++ src/main/kotlin/chlim/creditaccount/domain/store/Store.kt
@@ -0,0 +1,60 @@
+package chlim.creditaccount.domain.store
+
+import chlim.creditaccount.common.AbstractEntity
+import chlim.creditaccount.domain.contact.Contact
+import chlim.creditaccount.domain.shared.PhoneNumber
+import chlim.creditaccount.domain.user.User
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+import javax.persistence.Table
+
+@Entity
+@Table(name = "stores")
+class Store(
+    name: String,
+    phoneNumber: String,
+    user: User,
+): AbstractEntity() {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    var name: String = name
+
+    var phoneNumber: PhoneNumber = PhoneNumber(phoneNumber)
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User = user
+
+    @OneToMany(mappedBy = "store")
+    var contacts: MutableList<Contact> = mutableListOf()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+
+        if (this.javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as Store
+
+        return this.id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return this.id?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return "Store=(id=${this.id}, name='${this.name}', phone='${this.phoneNumber})"
+    }
+}
