10cc3cf2650ec847454809c275639d5f47fca300
chaehwanlim <chlim428@gmail.com>
2023-07-14 23:37:02 +0900

    feat: add usecase

---
+++ src/main/kotlin/chlim/creditaccount/domain/user/usecase/CreateUser.kt
@@ -0,0 +1,38 @@
+package chlim.creditaccount.domain.user.usecase
+
+import chlim.creditaccount.domain.user.User
+import chlim.creditaccount.domain.user.exception.UserAlreadyExistsException
+import chlim.creditaccount.domain.user.repository.UserRepository
+import chlim.creditaccount.domain.user.usecase.command.CreateUserCommand
+import chlim.creditaccount.domain.user.usecase.result.UserResult
+import org.springframework.stereotype.Service
+import javax.transaction.Transactional
+
+@Service
+class CreateUser(
+    private val userRepository: UserRepository
+) {
+
+    @Transactional
+    fun execute(command: CreateUserCommand): UserResult {
+        if (userRepository.findByPhoneNumber(command.phoneNumber) != null) {
+            throw UserAlreadyExistsException(command.phoneNumber)
+        }
+
+        val user = run {
+            val user = User(
+                name = command.name,
+                email = command.email,
+                phoneNumber = command.phoneNumber
+            )
+
+            userRepository.save(user)
+        }
+
+        return UserResult(
+            name = user.name,
+            email = user.email,
+            phoneNumber = user.phoneNumber.value
+        )
+    }
+}
