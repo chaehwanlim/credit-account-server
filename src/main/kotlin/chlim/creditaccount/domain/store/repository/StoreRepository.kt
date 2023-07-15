d5f45edf52560868e150d8d77f89ee8399e8f312
chaehwanlim <chlim428@gmail.com>
2023-07-15 23:00:04 +0900

    feat: add findByUserAndName method

--- src/main/kotlin/chlim/creditaccount/domain/store/repository/StoreRepository.kt
+++ src/main/kotlin/chlim/creditaccount/domain/store/repository/StoreRepository.kt
@@ -1,6 +1,10 @@
 package chlim.creditaccount.domain.store.repository
 
 import chlim.creditaccount.domain.store.Store
+import chlim.creditaccount.domain.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface StoreRepository: JpaRepository<Store, Long> {}
+interface StoreRepository: JpaRepository<Store, Long> {
+
+    fun findByUserAndName(user: User, name: String): Store?
+}
