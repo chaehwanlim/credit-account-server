789d696196e841421cca078f1f44ee4738e73b07
chaehwanlim <chlim428@gmail.com>
2023-07-17 15:05:56 +0900

    feat: add findByStoreAndName repository method

--- src/main/kotlin/chlim/creditaccount/domain/product/repository/ProductRepository.kt
+++ src/main/kotlin/chlim/creditaccount/domain/product/repository/ProductRepository.kt
@@ -1,6 +1,10 @@
 package chlim.creditaccount.domain.product.repository
 
 import chlim.creditaccount.domain.product.Product
+import chlim.creditaccount.domain.store.Store
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProductRepository: JpaRepository<Product, Long> {}
+interface ProductRepository: JpaRepository<Product, Long> {
+
+    fun findByStoreAndName(store: Store, name: String): Product?
+}
