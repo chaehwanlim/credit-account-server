3f1bd4f78214e12f4afecf19eed39204d67bd23e
chaehwanlim <chlim428@gmail.com>
2023-07-18 23:40:16 +0900

    feat: add command

---
+++ src/main/kotlin/chlim/creditaccount/domain/contact/usecase/command/CreateContactCommand.kt
@@ -0,0 +1,17 @@
+package chlim.creditaccount.domain.contact.usecase.command
+
+import chlim.creditaccount.common.SelfValidator
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Positive
+
+data class CreateContactCommand(
+    @field: NotBlank val name: String,
+    @field: NotBlank val phoneNumber: String,
+    val memo: String?,
+    @field: Positive val storeId: Long
+): SelfValidator<CreateContactCommand>() {
+
+    init {
+        validateAndIfViolatedThrows()
+    }
+}
