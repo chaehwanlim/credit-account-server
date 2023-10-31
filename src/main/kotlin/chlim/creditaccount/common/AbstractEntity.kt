904127abadfd5912ea0b449bfabe36f7d727d155
Chaehwan Lim <chlim428@gmail.com>
2023-10-31 17:14:30 +0900

    fix: abstract entity auditing

--- src/main/kotlin/chlim/creditaccount/common/AbstractEntity.kt
+++ src/main/kotlin/chlim/creditaccount/common/AbstractEntity.kt
@@ -1,23 +1,28 @@
 package chlim.creditaccount.common
 
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
+import jakarta.persistence.*
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.ZonedDateTime
-import jakarta.persistence.Column
-import jakarta.persistence.EntityListeners
-import jakarta.persistence.MappedSuperclass
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class AbstractEntity(
 
-    @CreatedDate
     @Column(nullable = false, updatable = false)
-    var createdDate: ZonedDateTime = ZonedDateTime.now(),
+    var createdAt: ZonedDateTime = ZonedDateTime.now(),
 
-    @LastModifiedDate
     @Column(nullable = false)
-    var lastModifiedDate: ZonedDateTime = ZonedDateTime.now()
+    var updatedAt: ZonedDateTime = ZonedDateTime.now()
+) {
 
-) {}
+    @PrePersist
+    fun prePersist() {
+        createdAt = ZonedDateTime.now()
+        updatedAt = ZonedDateTime.now()
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        updatedAt = ZonedDateTime.now()
+    }
+}
